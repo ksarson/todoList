56ec7027bc9625f5b014b1c801f28229521b0be1
ksarson <37591778+ksarson@users.noreply.github.com>
2024-02-06 22:20:03 -0500

    Porjects Page Functional + Array Stored Divs

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,7 @@
 //#region - Constants
+const currentProjectsArray = [];
+const completedProjectsArray = [];
+
 const projectsMessage = `<h2>Welcome to the Projects Page!</h2>
 Explore the heart of your productivity journey with our Projects feature. 
 Seamlessly create, organize, and track your projects to bring your ideas to life. 
@@ -63,7 +66,7 @@ function createWidget(headerText) {
 }
 
 function createList(headerText) {
-    const newList = document.createElement('ul');
+    const newList = document.createElement('div');
     newList.classList.add(`${headerText}-list`.replace(' ', '-').toLowerCase());
     newList.setAttribute(
         'id',
@@ -78,59 +81,123 @@ function addListItem() {
         'current-projects-list'
     );
 
-    const newProjectItem = document.createElement('li');
+    const newProjectItem = document.createElement('div');
     newProjectItem.classList.add('project-list-item');
-    newProjectItem.textContent = 'New Project';
+
+    const textElement = document.createElement('div');
+    textElement.classList.add('project-list-text');
+    textElement.textContent = 'New Project';
+
+    newProjectItem.appendChild(textElement);
 
     const itemActions = document.createElement('div');
     itemActions.classList.add('list-item-actions');
 
-    // #region Complete Button
-    const completeButton = document.createElement('button');
-    completeButton.classList.add('complete-button');
+    //#region Create Buttons
+    const moveButton = createButton(
+        './img/completeIcon.svg',
+        'Move',
+        'move-button'
+    );
+    moveButton.addEventListener('click', moveButtonHandler);
 
-    const completeIcon = document.createElement('img');
-    completeIcon.src = './img/completeIcon.svg';
-    completeIcon.alt = 'Delete';
-    completeIcon.style.height = '24px';
-    completeIcon.style.width = '24px';
+    const editButton = createButton(
+        './img/editIcon.svg',
+        'Edit',
+        'edit-button'
+    );
+    editButton.addEventListener('click', editButtonHandler);
 
-    completeButton.appendChild(completeIcon);
-    // #endregion
+    const deleteButton = createButton(
+        './img/trashIcon.png',
+        'Delete',
+        'delete-button'
+    );
+    deleteButton.addEventListener('click', deleteButtonHandler);
+    //#endregion
 
-    // #region Edit Button
-    const editButton = document.createElement('button');
-    editButton.classList.add('edit-button');
-
-    const editIcon = document.createElement('img');
-    editIcon.src = './img/editIcon.svg';
-    editIcon.alt = 'Edit';
-    editIcon.style.height = '24px';
-    editIcon.style.width = '24px';
-
-    editButton.appendChild(editIcon);
-    // #endregion
-
-    // #region Delete Button
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete-button');
-
-    const trashBinIcon = document.createElement('img');
-    trashBinIcon.src = './img/trashIcon.png';
-    trashBinIcon.alt = 'Delete';
-    trashBinIcon.style.height = '24px';
-    trashBinIcon.style.width = '24px';
-
-    deleteButton.appendChild(trashBinIcon);
-    // #endregion
-
-    itemActions.appendChild(completeButton);
+    itemActions.appendChild(moveButton);
     itemActions.appendChild(editButton);
     itemActions.appendChild(deleteButton);
     newProjectItem.appendChild(itemActions);
     currentProjectsList.appendChild(newProjectItem);
+    currentProjectsArray.push(newProjectItem);
 
     return currentProjectsList;
+}
+
+function createButton(iconSrc, altText, buttonClass) {
+    const button = document.createElement('button');
+    button.classList.add(buttonClass);
+
+    const icon = document.createElement('img');
+    icon.src = iconSrc;
+    icon.alt = altText;
+    icon.style.height = '24px';
+    icon.style.width = '24px';
+
+    button.appendChild(icon);
+
+    return button;
+}
+
+function moveButtonHandler() {
+    const listItem = this.closest('.project-list-item');
+    if (listItem) {
+        const currentList = document.getElementById('current-projects-list');
+        const completedList = document.getElementById(
+            'completed-projects-list'
+        );
+        const clonedItem = listItem.cloneNode(true);
+        const moveIcon = clonedItem.querySelector('.move-button img');
+        if (listItem.parentNode === currentList) {
+            moveIcon.alt = 'Uncomplete';
+            moveIcon.src = './img/xIcon.svg';
+            completedProjectsArray.push(listItem);
+            currentProjectsArray.splice(
+                currentProjectsArray.indexOf(listItem),
+                1
+            );
+            completedList.appendChild(clonedItem);
+        } else if (listItem.parentNode === completedList) {
+            moveIcon.alt = 'Complete';
+            moveIcon.src = './img/completeIcon.svg';
+            currentProjectsArray.push(listItem);
+            completedProjectsArray.splice(
+                completedProjectsArray.indexOf(listItem),
+                1
+            );
+            currentList.appendChild(clonedItem);
+        }
+        listItem.remove();
+
+        const clonedMoveButton = clonedItem.querySelector('.move-button');
+        clonedMoveButton.addEventListener('click', moveButtonHandler);
+        const clonedEditButton = clonedItem.querySelector('.edit-button');
+        clonedEditButton.addEventListener('click', editButtonHandler);
+        const clonedDeleteButton = clonedItem.querySelector('.delete-button');
+        clonedDeleteButton.addEventListener('click', deleteButtonHandler);
+    }
+}
+
+function editButtonHandler() {
+    const listItem = this.closest('.project-list-item');
+    if (listItem) {
+        const textElement = listItem.querySelector('.project-list-text');
+        if (textElement) {
+            const newText = prompt('Enter new text:');
+            if (newText !== null) {
+                textElement.textContent = newText;
+            }
+        }
+    }
+}
+
+function deleteButtonHandler() {
+    const listItem = this.closest('.project-list-item');
+    if (listItem) {
+        listItem.remove();
+    }
 }
 
 function createProjects() {
